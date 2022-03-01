e300ec4930a0d0f468703f293cdd366339e0a22d
Ao Li <leeleo3x@gmail.com>
2022-03-01 09:59:59 -0500

    update.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ else
   sudo apt install -y neovim byobu zsh
 fi
 
-sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -sLf https://spacevim.org/install.sh | bash
 pip3 install --user pylint yapf autoflake flake8 pyflakes
