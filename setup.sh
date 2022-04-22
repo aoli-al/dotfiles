2ed543d3281ef2c3969ebcb4e8f58ee70de43eaa
Ao Li <leeleo3x@gmail.com>
2022-04-22 12:33:23 -0400

    update.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,10 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install neovim
   brew install byobu
 else 
-  sudo apt update
-  sudo apt upgrade -y
+  # sudo apt update
+  # sudo apt upgrade -y
   sudo apt install -y neovim byobu zsh
 fi
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -sLf https://spacevim.org/install.sh | bash
 pip3 install --user pylint yapf autoflake flake8 pyflakes
--- spacevim/neomake_python_config.vim
+++
@@ -1,3 +0,0 @@
-function! neomake_python_config#before() abort
-    let g:neomake_python_python_exe = 'python3'
-endf
